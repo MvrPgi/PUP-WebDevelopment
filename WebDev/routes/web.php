6ad9e217d0ec52702acf4d7ea59f96f5f8b614a0
Mover <movermercene@gmail.com>
2024-12-11 15:39:48 +0800

    added packages

--- WebDev/routes/web.php
+++ WebDev/routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/home', function () {
+    return "Sample Ui";
+})->name('homePage');
+
+Route::get('/about', function () {
+    return"<a href ='" . route('homePage'). "'>Home</a>";
+});
